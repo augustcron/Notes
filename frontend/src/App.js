b726c3e71790bbf5d97d329a98cde408a09befb5
August <augustcron@gmail.com>
2024-03-01 22:41:34 +0300

    Заработало, перенес кнопку добавления в список заметок

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import AddNote from "./components/AddNote.jsx";
 import ListNotes from "./components/ListNotes.jsx";
 
 import React from 'react';
@@ -8,7 +7,6 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <AddNote />
         <ListNotes />
       </header>
     </div>
--- frontend/src/components/ListNotes.jsx
+++ frontend/src/components/ListNotes.jsx
@@ -1,6 +1,9 @@
 import Note from "./Note";
 import { API_URL } from "../index.js";
 import axios from "axios";
+import Button from 'react-bootstrap/Button';
+import Collapse from 'react-bootstrap/Collapse';
+import Form from 'react-bootstrap/Form';
 import { useState, useEffect } from "react";
 
 export default function ListNotes() {
@@ -17,11 +20,72 @@ export default function ListNotes() {
 
   useEffect(() => {
     fetchNotes();
-  }, [notes]); // Вызывается только после первой отрисовки
+  }, []);
 
+  const [open, setOpen] = useState(false);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleDescriptionChange = (event) => {
+    setDescription(event.target.value);
+  };
+
+  const handleNoteSubmission = async () => {
+    try {
+      const response = await axios.post(API_URL, { name, description });
+      fetchNotes()
+      setOpen(!open)
+    } catch (error) {
+      // Handle any errors that occur during the submission
+    }
+  };
 
   return (
     <>
+      <div>
+        <Button
+          onClick={() => setOpen(!open)}
+          aria-controls="example-collapse-text"
+          aria-expanded={open}
+        >
+          Add One
+        </Button>
+
+        <Collapse in={open}>
+          <div id="example-collapse-text">
+            <Form>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={name}
+                  onChange={handleNameChange}
+                />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Label>Description</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  rows={3}
+                  value={description}
+                  onChange={handleDescriptionChange}
+                />
+              </Form.Group>
+              <Button onClick={handleNoteSubmission}>Submit Note</Button>
+            </Form>
+          </div>
+        </Collapse>
+      </div>
       {notes.map((note) => (
         <Note key={note.id} {...note} />
       ))}
